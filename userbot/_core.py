435e5e7f018c9cb89ec88b4ac320de18e36dea7c
sameerpanthi <75129693+sameerpanthi@users.noreply.github.com>
2021-05-31 12:08:22 +0530

    Update _core.py

--- userbot/_core.py
+++ userbot/_core.py
@@ -11,9 +11,9 @@ from userbot import bot as mafiabot
 
 DELETE_TIMEOUT = 5
 DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Mafia User"
-mafia_logo = "./H1M4N5HU0P/SAVAGE_BOT_LOGO.jpg"
-h1m4n5hu0p = mafiabot.uid
-mafia = f"[{DEFAULTUSER}](tg://user?id={h1m4n5hu0p})"
+mafia_logo = "./SAVAGE_SAMEER/SAVAGE_BOT_LOGO.jpg
+SAVAGE_SAMEER = SAVAGE.uid
+SAVAGE_SAMEER= f"[{DEFAULTUSER}](tg://user?id={SAVAGE_SAMEER})"
 
 @mafiabot.on(admin_cmd(pattern=r"send (?P<shortname>\w+)", outgoing=True))
 @mafiabot.on(sudo_cmd(pattern=r"send (?P<shortname>\w+)", allow_sudo=True))
@@ -23,7 +23,7 @@ async def send(event):
     message_id = event.message.id
     thumb = mafia_logo
     input_str = event.pattern_match.group(1)
-    omk = f"𝖯𝙻𝚄𝙶𝙸𝙽 𝙽𝙰𝙼𝙴 ➪ `{input_str}`\n𝖴𝙿𝙻𝙾𝙰𝙳𝙴𝙳 𝙱𝚈 ➪ {mafia}\n\n[𝚂𝙰𝚅𝙰𝙶𝙴 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 ©](t.me/savage_userbot)"
+    omk = f"𝖯𝙻𝚄𝙶𝙸𝙽 𝙽𝙰𝙼𝙴 ➪ `{input_str}`\n𝖴𝙿𝙻𝙾𝙰𝙳𝙴𝙳 𝙱𝚈 ➪ {sameer}\n\n[𝚂𝙰𝚅𝙰𝙶𝙴 𝚄𝚂𝙴𝚁𝙱𝙾𝚃 ©](t.me/savage_userbot)"
     the_plugin_file = "./userbot/plugins/{}.py".format(input_str)
     if os.path.exists(the_plugin_file):
         lauda = await event.client.send_file(
@@ -39,8 +39,8 @@ async def send(event):
     else:
         await edit_or_reply(event, "File not found..... Kek")
 
-@mafiabot.on(admin_cmd(pattern="install$", outgoing=True))
-@mafiabot.on(sudo_cmd(pattern="install$", allow_sudo=True))
+@SAVAGE.on(admin_cmd(pattern="install$", outgoing=True))
+@SAVAGE.on(sudo_cmd(pattern="install$", allow_sudo=True))
 async def install(event):
     if event.fwd_from:
         return
@@ -71,7 +71,7 @@ async def install(event):
                             a = "__Installing...__"
                             b = 1
                         await event.edit(a)
-                    return await event.edit(f"✅ **Installed module** :- `{shortname}` \n✨ BY :- {mafia}\n\n{string}\n\n        ⚡ **[Lᴇɢᴇɴᴅʀʏ Aғ Sᴀᴠᴀɢᴇ Bᴏᴛ](t.me/SAVAGE_USERBOT)** ⚡", link_preview=False)
+                    return await event.edit(f"✅ **Installed module** :- `{shortname}` \n✨ BY :- {sameer}\n\n{string}\n\n        ⚡ **[Lᴇɢᴇɴᴅʀʏ Aғ Sᴀᴠᴀɢᴇ Bᴏᴛ](t.me/SAVAGE_USERBOT)** ⚡", link_preview=False)
                 return await event.edit(f"Installed module `{os.path.basename(downloaded_file_name)}`")
             else:
                 os.remove(downloaded_file_name)
@@ -80,22 +80,22 @@ async def install(event):
             await event.edit(f"**Failed to Install** \n`Error`\n{str(e)}")
             return os.remove(downloaded_file_name)
     
-@mafiabot.on(admin_cmd(pattern=r"uninstall (?P<shortname>\w+)", outgoing=True))
-@mafiabot.on(sudo_cmd(pattern=r"uninstall (?P<shortname>\w+)", allow_sudo=True))
+@SAVAGE.on(admin_cmd(pattern=r"uninstall (?P<shortname>\w+)", outgoing=True))
+@SAVAGE.on(sudo_cmd(pattern=r"uninstall (?P<shortname>\w+)", allow_sudo=True))
 async def uninstall(h1m4n5hu0p):
-    if h1m4n5hu0p.fwd_from:
+    if SAVAGE_SAMEER.fwd_from:
         return
-    shortname = h1m4n5hu0p.pattern_match["shortname"]
+    shortname = SAVAGE_SAMEER.pattern_match["shortname"]
     dir_path =f"./userbot/plugins/{shortname}.py"
     try:
         remove_plugin(shortname)
         os.remove(dir_path)
-        await h1m4n5hu0p.edit(f"Uninstalled `{shortname}` successfully")
+        await SAVAGE_SAMEER.edit(f"Uninstalled `{shortname}` successfully")
     except OSError as e:
-        await h1m4n5hu0p.edit("Error: %s : %s" % (dir_path, e.strerror))
+        await SAVAGE_SAMEER.edit("Error: %s : %s" % (dir_path, e.strerror))
 
-@mafiabot.on(admin_cmd(pattern=r"unload (?P<shortname>\w+)$"))
-@mafiabot.on(sudo_cmd(pattern=r"upload (?P<shortname>\w+)$", allow_sudo=True))
+@SAVAGE.on(admin_cmd(pattern=r"unload (?P<shortname>\w+)$"))
+@SAVAGE.on(sudo_cmd(pattern=r"upload (?P<shortname>\w+)$", allow_sudo=True))
 async def unload(event):
     if event.fwd_from:
         return
@@ -111,8 +111,8 @@ async def unload(event):
         )
 
 
-@mafiabot.on(admin_cmd(pattern=r"load (?P<shortname>\w+)$"))
-@mafiabot.on(sudo_cmd(pattern=r"load (?P<shortname>\w+)$", allow_sudo=True))
+@SAVAGE.on(admin_cmd(pattern=r"load (?P<shortname>\w+)$"))
+@SAVAGE.on(sudo_cmd(pattern=r"load (?P<shortname>\w+)$", allow_sudo=True))
 async def load(event):
     if event.fwd_from:
         return
